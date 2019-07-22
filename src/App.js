ff6b61dbb4a67f3cf60cdb1731d6fde78f9bbaa2
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-07-22 18:27:04 -0400

    got my app working after a slower start reviewing the material

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
+import { Route, Switch } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/About" component={About} />
+      <Route path="/Contact" component={Contact} />
+    </Switch>
   </div>
 );
 
