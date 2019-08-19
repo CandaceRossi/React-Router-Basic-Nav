218c6b46d8384e328e5bd8a43cec2bb42473dd1e
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-19 17:42:47 -0400

    set up project for re-do

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 import "./App.css";
 import { Home, About, Contact, Navigation } from "./components";
-import { Route, Switch } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
-
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/About" exact component={About} />
-      <Route path="/Contact" exact component={Contact} />
-    </Switch>
   </div>
 );
 
